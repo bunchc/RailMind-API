751e14aff0ee7f13936d36e2109a273603e652de
Cody Bunch <bunchc@gmail.com>
2023-12-09 14:38:20 -0600

    ugh flak8

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
+# app/__init__.py
+
 from flask import Flask
 from flask_restful import Api
+from RPiMotorLib import RPiMotorDC
 
 app = Flask(__name__)
 api = Api(app)
 
-from app import routes
+# Initialize the DC motors for each train
+trains = [
+    {'id': 1, 'name': 'Express Train', 'speed': 0, 'direction': 'forward'},
+    {'id': 2, 'name': 'Freight Train', 'speed': 0, 'direction': 'forward'},
+]
+
+motor_channels = [f"MOTOR{i}" for i in range(1, len(trains) + 1)]
+motors = {train['id']: RPiMotorDC(channel, 200) for train, channel in zip(trains, motor_channels)}
+
+from app import routes  # noqa
